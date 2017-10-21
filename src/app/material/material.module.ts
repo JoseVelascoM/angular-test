66177caed48328b302fc07953e1abbf1a69dc952
JVMTBB <josepe_789@hotmail.com>
2017-10-21 09:25:03 -0400

    Project created and angular material installed

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  MATERIAL_COMPATIBILITY_MODE,
+  MatButtonModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatListModule,
+  MatProgressBarModule
+} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatProgressBarModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatListModule,
+    MatProgressBarModule
+  ],
+  providers: [
+    { provide: MATERIAL_COMPATIBILITY_MODE, useValue: true }
+  ],
+  declarations: []
+})
+export class MaterialModule { }
